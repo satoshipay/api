968d05b8b93efeb3d503b59b8b083ba4743475de
meinhard <meinhard@benn.org>
2016-05-15 16:27:59 +0200

    cp instead of sync

--- publish.sh
+++ publish.sh
@@ -3,5 +3,5 @@ DEFAULT="satoshipay-nano"
 PROFILE=${AWS_PROFILE:-$DEFAULT}
 BUCKET="docs.satoshipay.io"
 REGION="eu-central-1"
+aws s3 cp index.html s3://$BUCKET/ --region $REGION
 aws s3 sync build s3://$BUCKET/api/ --region $REGION
-aws s3 sync index.html s3://$BUCKET/ --region $REGION
